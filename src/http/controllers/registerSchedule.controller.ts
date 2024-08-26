dc9f6543a5f1aa06786b3ec1f7be57519b1ee167
WevertonCintra <wevertoncintra@outlook.com>
2024-08-26 00:04:00 -0300

    feat: add register schedule endpoint

---
+++ src/http/controllers/registerSchedule.controller.ts
@@ -0,0 +1,46 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+
+import { registerScheduleUseCaseFactory } from '@/useCases/factories/registerScheduleUseCase.factory'
+import { ScheduleAlreadyExistsError } from '@/errors/scheduleAlreadyExists.error'
+
+export async function registerScheduleController(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const register_params_schema = z.object({
+    clinic_id: z.string().uuid()
+  })
+
+  const register_body_schema = z.object({
+    patient_id: z.string().uuid(),
+    service_id: z.string().uuid(),
+    date: z.string()
+  })
+
+  const { clinic_id } = register_params_schema.parse(request.params)
+  
+  const { patient_id, service_id, date } = register_body_schema.parse(request.body)
+
+  const user_id = request.user.sub
+
+  try {
+    const registerScheduleUseCase = registerScheduleUseCaseFactory()
+
+    await registerScheduleUseCase.execute({
+      user_id,
+      clinic_id,
+      patient_id,
+      service_id,
+      date
+    })
+
+    return reply.status(201).send()
+  } catch (error) {
+    if (error instanceof ScheduleAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
+  }
+}
---
+++ src/useCases/registerSchedule.useCase.ts
@@ -0,0 +1,57 @@
+import { Schedule } from '@prisma/client'
+
+import { SchedulesRepository } from '@/repositories/interfaces/schedules.repository'
+import { PatientsRepository } from '@/repositories/interfaces/patients.repository'
+import { ClinicsRepository } from '@/repositories/interfaces/clinics.repository'
+import { ServicesRepository } from '@/repositories/interfaces/services.repository'
+import { ResourceNotFoundError } from '@/errors/resourceNotFound.error' 
+
+interface IRequest {
+  user_id: string
+  clinic_id: string
+  patient_id: string
+  service_id: string
+  date: string
+}
+
+interface IResponse {
+  schedule: Schedule
+}
+
+export class RegisterScheduleUseCase {
+  constructor(
+    private schedulesRepository: SchedulesRepository, 
+    private patientsRepository: PatientsRepository, 
+    private clinicsRepository: ClinicsRepository, 
+    private servicesRepository: ServicesRepository
+  ) {}
+
+  async execute({ user_id, clinic_id, patient_id, service_id, date }: IRequest): Promise<IResponse> {
+    const clinic = await this.clinicsRepository.findByIdAndUserId(clinic_id, user_id)
+
+    if (!clinic) {
+      throw new ResourceNotFoundError()
+    }
+
+    const patient = await this.patientsRepository.findByIdAndClinicId(patient_id, clinic_id)
+
+    if (!patient) {
+      throw new ResourceNotFoundError()
+    }
+
+    const service = await this.servicesRepository.findByIdAndClinicId(service_id, clinic_id)
+
+    if (!service) {
+      throw new ResourceNotFoundError()
+    }
+
+    const schedule = await this.schedulesRepository.create({
+      clinic_id,
+      patient_id,
+      service_id,
+      date
+    })
+
+    return { schedule }
+  }
+}
