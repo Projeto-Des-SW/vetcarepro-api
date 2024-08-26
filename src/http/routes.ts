be5eb29d5a1900b0846b5fba29d28d5a6522d6b2
WevertonCintra <wevertoncintra@outlook.com>
2024-08-26 00:04:41 -0300

    refac: adjust structure routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,23 @@
 import { FastifyInstance } from 'fastify'
 
+import { jwtMiddleware } from '@/http/middlewares/jwt.middleware'
 import { authenticateUserController } from './controllers/authenticateUser.controller'
 import { registerUserController } from './controllers/registerUser.controller'
+import { profileController } from './controllers/profile.controller'
+import { registerClinicController } from './controllers/registerClinic.controller'
+import { registerServiceController } from './controllers/registerService.controller'
+import { registerPatientController } from './controllers/registerPatient.controller'
+import { registerScheduleController } from './controllers/registerSchedule.controller'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticateUserController)
   app.post('/users', registerUserController)
+
+  app.addHook('onRequest', jwtMiddleware)
+
+  app.get('/users/me', profileController)
+  app.post('/clinics', registerClinicController)
+  app.post('/clinics/:clinic_id/services', registerServiceController)
+  app.post('/clinics/:clinic_id/patients', registerPatientController)
+  app.post('/clinics/:clinic_id/schedules', registerScheduleController)
 }
